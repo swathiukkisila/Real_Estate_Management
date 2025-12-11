4bcf7c9a10fd4e2f22f250ace459eead521b0056
swathi ukkisila <swathi76750@gmail.com>
2025-12-11 20:54:51 +0530

    code updated

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,7 +69,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        // secure:true,
+         secure:true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(200)
